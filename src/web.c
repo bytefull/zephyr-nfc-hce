bf0d84460602a9fc7d833ae319a64d626deb32d3
bayrem-gharsellaoui <bayrem.gharsellaoui@gmail.com>
2026-05-17 14:02:16 +0000

    offloaded sockets

--- src/web.c
+++ src/web.c
@@ -3,12 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-
-#include <stdio.h>
-#include <inttypes.h>
-
 #include <zephyr/kernel.h>
-#include <zephyr/net/net_config.h>
 #include <zephyr/net/http/server.h>
 #include <zephyr/net/http/service.h>
 #include <zephyr/logging/log.h>
@@ -18,26 +13,25 @@ LOG_MODULE_REGISTER(web, LOG_LEVEL_DBG);
 
 static void web_thread_entry(void *p1, void *p2, void *p3);
 
-K_THREAD_DEFINE(web_thread, 1024, web_thread_entry, NULL, NULL, NULL, 8, 0, 0);
+K_THREAD_DEFINE(web_thread, 4096, web_thread_entry, NULL, NULL, NULL, 8, 0, 0);
 
-static const uint16_t http_service_port = 80;
-static const uint8_t index_html_gz[] = {
-    #include "index.html.gz.inc"
-};
+static const uint16_t http_service_port = 8082;
+
+static const uint8_t json_message[] = "{\"message\":\"HTTP server is running\"}";
 
 HTTP_SERVICE_DEFINE(my_service, "0.0.0.0", &http_service_port, 1, 10, NULL, NULL, NULL);
 
-static const struct http_resource_detail_static index_html_gz_resource_detail = {
+static const struct http_resource_detail_static json_message_resource_detail = {
     .common = {
         .type = HTTP_RESOURCE_TYPE_STATIC,
         .bitmask_of_supported_http_methods = BIT(HTTP_GET),
-        .content_encoding = "gzip",
+        .content_type = "application/json",
     },
-    .static_data = index_html_gz,
-    .static_data_len = sizeof(index_html_gz),
+    .static_data = json_message,
+    .static_data_len = sizeof(json_message) - 1,
 };
 
-HTTP_RESOURCE_DEFINE(index_html_gz_resource, my_service, "/", &index_html_gz_resource_detail);
+HTTP_RESOURCE_DEFINE(json_message_resource, my_service, "/", &json_message_resource_detail);
 
 static void web_thread_entry(void *p1, void *p2, void *p3)
 {
@@ -47,6 +41,7 @@ static void web_thread_entry(void *p1, void *p2, void *p3)
 
 	/* Block thread to wait for getting an IP address from the network */
 	wait_for_network();
+	LOG_INF("Network is ready, starting HTTP server...");
 
 	http_server_start();
 
