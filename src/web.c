bb237694fd0f86e5dea9fd72c9eb1f1780f061a9
bayrem-gharsellaoui <bayrem.gharsellaoui@gmail.com>
2026-05-17 21:01:56 +0000

    serving compressed html is working

--- src/web.c
+++ src/web.c
@@ -15,11 +15,24 @@ static void web_thread_entry(void *p1, void *p2, void *p3);
 
 K_THREAD_DEFINE(web_thread, 4096, web_thread_entry, NULL, NULL, NULL, 8, 0, 0);
 
-static const uint16_t http_service_port = 8082;
-
+static const uint8_t index_html_gz[] = {
+    #include "index.html.gz.inc"
+};
 static const uint8_t json_message[] = "{\"message\":\"HTTP server is running\"}";
 
+static const uint16_t http_service_port = 8080;
 HTTP_SERVICE_DEFINE(my_service, "0.0.0.0", &http_service_port, 1, 10, NULL, NULL, NULL);
+
+static struct http_resource_detail_static index_html_gz_resource_detail = {
+    .common = {
+        .type = HTTP_RESOURCE_TYPE_STATIC,
+        .bitmask_of_supported_http_methods = BIT(HTTP_GET),
+        .content_encoding = "gzip",
+    },
+    .static_data = index_html_gz,
+    .static_data_len = sizeof(index_html_gz),
+};
+HTTP_RESOURCE_DEFINE(index_html_gz_resource, my_service, "/", &index_html_gz_resource_detail);
 
 static struct http_resource_detail_static json_message_resource_detail = {
     .common = {
@@ -30,8 +43,7 @@ static struct http_resource_detail_static json_message_resource_detail = {
     .static_data = json_message,
     .static_data_len = sizeof(json_message) - 1,
 };
-
-HTTP_RESOURCE_DEFINE(json_message_resource, my_service, "/", &json_message_resource_detail);
+HTTP_RESOURCE_DEFINE(json_message_resource, my_service, "/data", &json_message_resource_detail);
 
 static void web_thread_entry(void *p1, void *p2, void *p3)
 {
